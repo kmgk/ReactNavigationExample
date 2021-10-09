dc145415e434c14d7cdeffc4b7421a5b422fe848
kmgk <hiragi.kame@gmail.com>
2021-10-10 02:17:45 +0900

    TabNavigatorのヘッダーを表示

--- App.tsx
+++ App.tsx
@@ -34,9 +34,6 @@ export default function App() {
         <HomeStack.Screen
           name="Tab"
           component={TabNavigator}
-          // options={{
-          //   headerShown: false
-          // }}
         />
       </HomeStack.Navigator>
     </NavigationContainer>
